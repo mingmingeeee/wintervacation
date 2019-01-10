e6122906ce32c81ae270951f402d31e268ebf3f0
mingmingeeee <45472076+mingmingeeee@users.noreply.github.com>
2019-01-10 17:24:49 +0900

    Add files via upload

--- 2-3.cpp
+++ 2-3.cpp
@@ -1,73 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#pragma warning(disable:4996)
-#include<stdio.h>
-#include<string.h>
-#define BUFFER_SIZE 1000
-char *words[BUFFER_SIZE];
-char write[BUFFER_SIZE];
-char buf[BUFFER_SIZE];
-char buf2[BUFFER_SIZE];
-int main() 
-{
-	int leng = 0, i = 0, jump = 0;
-	FILE *fp = fopen("har.txt", "r");
-	FILE *fp_out = fopen("words2.txt", "w");
-	while (1) {
-		if (leng < 80) {
-			strcpy(buf2, buf);
-			leng += strlen(buf) + 1;
-			words[i] = strdup(buf);
-			//마지막줄
-			if (fscanf(fp, "%s", buf) == EOF) {
-				for (int j = 0; j < i+1; j++) {
-					strcat(write, words[j]);
-					if (j < i)
-						strcat(write, " ");
-				}
-				fprintf(fp_out, "%s\n", write);
-				break;
-			}
-			//
-			i++;
-		}
-		else if (leng == 80) {
-				i--;
-			for (int j = 0; j < i+1 ; j++) {
-				strcat(write, words[j]);
-				if (j == 0)
-					strcat(write, "  ");
-				else if (j < i)
-					strcat(write, " ");
-			}
-			fprintf(fp_out, "%s\n", write);
+#define MAX 80
 
-			for (int i = 0; i < BUFFER_SIZE; i++)
-				words[i] = { '\0' };
-			i = 1, leng = strlen(buf)+1, write[0] = {'\0'};
-			words[0] = strdup(buf);
+int main() {
+
+	FILE  *fp1 = fopen("harry.txt", "r");
+	FILE  *fp2 = fopen("aligned.txt", "w");
+
+	char ch; //단어 입력
+	char buffer[MAX]; //입력파일 한 줄
+	char * words[MAX]; //출력파일 한 줄 -> 단어들
+	char *token;  //토큰
+
+	int words_i = 0, length = 0; //단어개수&글자수
+	int index = 0; //입력파일 index
+	int flag = 0;
+	while (feof(fp1) == 0) //한단어씩 입력받는다
+	{
+		if (flag == 1) {
+			index = 0;
+			flag = 0;
 		}
-		else {//		
-			jump = 80 - (leng - strlen(buf2) - 1);
-			i--;
-			for (int j = 0; j < i ; j++) {
-				strcat(write, words[j]);
-				if (jump >=0&&j<i) {
-					strcat(write,"  ");
-					jump--;
+		fscanf(fp1, "%c", &ch);
+		buffer[index++] = ch; //buffer라는 배열을 만들어서 저장함 -> 입력파일의 한 줄
+
+		if (ch == '\n') { //줄바꿈 문자가 나올 때 
+			flag = 1;
+			buffer[index - 1] = '\0'; //입력파일에서의 한 줄을 구분해주기 위해 널문자를 넣어서 끝을 구분해줌
+
+			token = strtok(buffer, " "); //-> 입력 파일의 한 줄이 끝났을 때 strtok 로 단어부터 나눠줌
+
+			while (token != NULL) { //입력파일의 한 줄을 나눔
+				length += strlen(token) + 1; //출력파일의 한 줄의 단어 개수 -> 마지막 단어에도 공백이 추가된 길이로 인식
+				words[words_i++] = strdup(token); //출력파일의 한 줄 단어 복사
+
+				if (length - 1 > 80) { //만약 length가 80보다 커지면
+					length -= strlen(token) + 2; //아까 마지막에도 공백 + 앞글자의 공백 고려해서 -2함
+					int L = (80 - length) / (words_i - 2);
+					for (int i = 0; i < words_i - 2; i++) {
+						fprintf(fp2, "%s", words[i]);
+						if ((L <= 0 && i < 80 - length) || (L > 0 && i < (80 - length) % (words_i - 2))) {
+							for (int j = 0; j <= L + 1; j++)
+								fprintf(fp2, " ");
+						}
+						else
+							for (int j = 0; j <= L; j++)
+								fprintf(fp2, " ");
+					}
+					fprintf(fp2, "%s\n", words[words_i - 2]); // 마지막 글자는 띄어쓰기 XX
+					length = strlen(token) + 1; //length 다시
+					words_i = 0; //초기화
+					words[words_i++] = strdup(token); //다시 복사
 				}
-				else if(j<i)
-					strcat(write," ");
+				token = strtok(NULL, " "); //출력파일 한 줄 나눠줌
 			}
-			fprintf(fp_out, "%s\n", write);
-			for (int i = 0; i < BUFFER_SIZE; i++)
-				words[i] = {'\0'};
-			i = 2, leng = strlen(buf) + 1+strlen(buf2)+1;
-			words[0] = strdup(buf2);
-			words[1] = strdup(buf);
-			buf2[0] = { '\0' };
-			write[0] = { '\0' };
 		}
 	}
-	fclose(fp), fclose(fp_out);
-	return 0;
+
+	// 마지막줄
+	for (int i = 0; i < words_i; i++)
+		fprintf(fp2, "%s ", words[i]);
+
+	fclose(fp1);
+	fclose(fp2);
+
 }
